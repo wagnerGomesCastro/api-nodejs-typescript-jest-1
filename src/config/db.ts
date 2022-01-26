758ec6e92ae33d2d62d5bb18df62fc1d91038f97
Wagner Castro <wagner2014.castro@gmail.com>
2022-01-26 12:19:12 -0300

    #01 Testando seeds

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,25 +3,25 @@
 require('dotenv').config();
 
 
-export const dbConfig = {
-  //dbConnection: env('TYPEORM_CONNECTION') || "mysql",
-  // dbConnection: "mysql",
-  type: "postgres",
-  // dbHost: process.env.TYPEORM_HOST,
-  // dbPort: process.env.TYPEORM_PORT,
-  // dbDatabase: process.env.TYPEORM_DATABASE,
-  // dbUsername: process.env.TYPEORM_USERNAME,
-  // dbPassword: process.env.TYPEORM_PASSWORD,
-  // dbEntities:  process.env.TYPEORM_ENTITIES, 
-  // allowLogging: process.env.TYPEORM_LOGGING,
+// export const dbConfig = {
+//   //dbConnection: env('TYPEORM_CONNECTION') || "mysql",
+//   // dbConnection: "mysql",
+//   type: "postgres",
+//   // dbHost: process.env.TYPEORM_HOST,
+//   // dbPort: process.env.TYPEORM_PORT,
+//   // dbDatabase: process.env.TYPEORM_DATABASE,
+//   // dbUsername: process.env.TYPEORM_USERNAME,
+//   // dbPassword: process.env.TYPEORM_PASSWORD,
+//   // dbEntities:  process.env.TYPEORM_ENTITIES, 
+//   // allowLogging: process.env.TYPEORM_LOGGING,
 
-  dbHost: "localhost",
-  dbPort: 3306,
-  dbDatabase: "express-typescript",
-  dbUsername: "kutia",
-  dbPassword: "6028201644",
-  // dbEntities:  "src/api/models/**/*.ts,src/api/models/**/*.js", 
-  // allowLogging: "src/database/migrations/*.ts,src/database/migrations/*.js",
+//   dbHost: "localhost",
+//   dbPort: 3306,
+//   dbDatabase: "express-typescript",
+//   dbUsername: "kutia",
+//   dbPassword: "6028201644",
+//   // dbEntities:  "src/api/models/**/*.ts,src/api/models/**/*.js", 
+//   // allowLogging: "src/database/migrations/*.ts,src/database/migrations/*.js",
 
-  synchronize: true,
-};
+//   synchronize: true,
+// };
